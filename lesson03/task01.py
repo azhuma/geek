6ad27bc187039313fa4a388bdc09dc20b802eab8
cnp.az <akylbek.zhumabayev@processing.kz>
2020-03-03 14:45:40 +0600

    task01 implemented.

--- lesson03/task01.py
+++ lesson03/task01.py
@@ -1,18 +1,17 @@
 def divide(v1, v2):
     try:
-        return v1 / v2, None
+        return v1 / v2
     except ZeroDivisionError:
-        return 0, "Zero division!"
+        print("Zero division!")
+        return
 
 
 a = input("a=")
 b = input("b=")
 
 if a.isnumeric() and b.isnumeric():
-    result, error = divide(int(a), int(b))
-    if error:
-        print(error)
-    else:
+    result = divide(int(a), int(b))
+    if result:
         print(f"result = {result:.2}")
 else:
     print("Invalid input!")
