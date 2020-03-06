b8ab76476c1807242d2cb6e873916a3062be0b20
cnp.az <akylbek.zhumabayev@processing.kz>
2020-03-06 17:53:50 +0600

    bug fix in task07.

--- lesson04/task07.py
+++ lesson04/task07.py
@@ -2,9 +2,9 @@ from functools import reduce
 
 
 def fact(n):
-    for i in range(1, n):
+    for i in range(1, n + 1):
         yield i
 
 
-result = reduce(lambda a, b: a + b, [el for el in fact(5)])
+result = reduce(lambda a, b: a * b, [el for el in fact(4)])
 print(result)
