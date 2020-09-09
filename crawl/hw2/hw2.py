002cf96cc6312dda888860642a67964fe7096999
cnp.az <akylbek.zhumabayev@processing.kz>
2020-09-09 23:39:42 +0600

    hw2 bugs fixed.

--- crawl/hw2/hw2.py
+++ crawl/hw2/hw2.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import re
 
 url = "https://hh.kz/search/vacancy"
 position = "python"
@@ -18,15 +19,25 @@ def get_jobs(params, headers, page=0):
     params.__setitem__('page', page)
     html = requests.get(url, params=params, headers=headers)
     dom = bs(html.text,'html.parser')
-    jobs = dom.find_all('div', {'class' : 'vacancy-serp-item__row_header'})
+    jobs = dom.find_all('div', {'class': 'vacancy-serp-item__row_header'})
 
     for job in jobs:
         title = job.find('a')
         print(f"Вакансия: {title.getText()}")
         print(f"Ссылка: {title.attrs['href']}")
-        sal = job.find('span', {'data-qa' : 'vacancy-serp__vacancy-compensation'})
+        sal = job.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
         if sal != None:
-            print(f"Зарплата: {sal}")
+            sv = sal.getText()
+            pat = re.compile(r'\s+')
+            sv = pat.sub('', sv)
+            sv_num = re.findall(r'\d+', sv)
+            curr = sv[-3:]
+            min = sv_num[0] if len(sv_num) > 0 else None
+            max = sv_num[1] if len(sv_num) > 1 else None
+            print(f"Зарплата: min={min}, max={max}, валюта={curr}")
 
-for page in (1,3):
-    get_jobs(params, headers, page)
+    next_page = dom.find('a', {'class': 'HH-Pager-Control', 'data-page': page + 1})
+    if next_page != None:
+        get_jobs(params, headers, page + 1)
+
+get_jobs(params, headers)
